daf88f780e937cd021e7909d35286948eebf9d3e
viniciussslima <vinicius.silva.017@ufrn.edu.br>
2021-07-10 18:40:02 -0300

    Implementing about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
-  return <div>Hello word!</div>;
+  return (
+    <div className="left-content">
+      <h1>Vinicius Santos Silva de Lima</h1>
+      <div className="about-text">
+        <p>Sou desenvolvedor de software focado em desenvolvimento WEB.</p>
+        <p>
+          Sempre estou em busca de me manter atualizado e aprender coisas novas.
+        </p>
+        <p>
+          Além de programar gosto de praticar esportes e sair com os amigos.
+        </p>
+        <p>
+          Atualmente estou cursando bacharelado em Tecnologia da informação na
+          Universidade Federal do Rio Grando do Norte (UFRN).
+        </p>
+        <p>
+          OBS: Essa foto minha foi tirada quando tentei usar tranças porém só
+          durou 20 dias. Ai você me pergunta, então porque escolheu essa? Bem...
+          Era a única que tinha um fundo facilmente removível.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
