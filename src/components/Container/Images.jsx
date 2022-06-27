f99af1ab1f12688557cac7d7e5bf46985268e80e
Vinicius Santos <viniciussslima>
2022-06-27 18:01:34 -0300

    Update tecnologes

--- src/components/Container/Images.jsx
+++ src/components/Container/Images.jsx
@@ -6,14 +6,14 @@ import cssLogo from "../../assets/tecnologes/css.svg";
 import dockerLogo from "../../assets/tecnologes/docker.svg";
 import gitLogo from "../../assets/tecnologes/git.svg";
 import htmlLogo from "../../assets/tecnologes/html.svg";
-import javaLogo from "../../assets/tecnologes/java.svg";
+import jestLogo from "../../assets/tecnologes/jest.svg";
 import javascriptLogo from "../../assets/tecnologes/js.svg";
 import mongodbLogo from "../../assets/tecnologes/mongodb.svg";
 import mysqlLogo from "../../assets/tecnologes/mysql.svg";
 import postgresLogo from "../../assets/tecnologes/postgres.svg";
 import reactLogo from "../../assets/tecnologes/react.svg";
 import redisLogo from "../../assets/tecnologes/redis.svg";
-import springLogo from "../../assets/tecnologes/spring.svg";
+import cypressLogo from "../../assets/tecnologes/cypress.svg";
 
 const Images = () => {
   return (
@@ -23,7 +23,7 @@ const Images = () => {
       <img className="logo dockerLogo" src={dockerLogo} alt="Docker Logo" />
       <img className="logo gitLogo" src={gitLogo} alt="Git Logo" />
       <img className="logo htmlLogo" src={htmlLogo} alt="HTML Logo" />
-      <img className="logo javaLogo" src={javaLogo} alt="Java Logo" />
+      <img className="logo jestLogo" src={jestLogo} alt="Java Logo" />
       <img
         className="logo javascriptLogo"
         src={javascriptLogo}
@@ -38,7 +38,7 @@ const Images = () => {
       />
       <img className="logo reactLogo" src={reactLogo} alt="React Logo" />
       <img className="logo redisLogo" src={redisLogo} alt="Redis Logo" />
-      <img className="logo springLogo" src={springLogo} alt="Spring Logo" />
+      <img className="logo cypressLogo" src={cypressLogo} alt="Cypress Logo" />
     </>
   );
 };
